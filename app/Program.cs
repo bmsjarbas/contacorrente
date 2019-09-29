d0d006a574f756fa3cd7526b86919c7e4e6236f3
Jarbas Brito <jbmsegundo@gmail.com>
2019-09-29 17:20:38 -0300

    adicionado menu console

--- app/Program.cs
+++ app/Program.cs
@@ -22,12 +22,52 @@ namespace contacorrente.app
             var lancamentosApi = RecuperarLancamentosApi();
             contaCorrente.IncluirMovimentacaoEmLote(lancamentosApi);
 
-            while(Console.ReadLine() != "0")
+
+            string operacao = "";
+            while (operacao != "0")
             {
-                
-            }           
+                ImprimirMenuInicial();
+                operacao = Console.ReadLine();
+                switch (operacao)
+                {
+                    case "0":
+                        break;
+                    case "1":
+                        ImprimirMovimentacaoOrdenada(contaCorrente.lancamentosOrdernadosPorData());
+                        PressioneParaContinuar();
+                        continue;
+                    case "2":
+                        ImprimirTotalDeGastosPorCategoria(contaCorrente.listarGastosPorCategoria());
+                        PressioneParaContinuar();
+                        continue;
+                    case "3":
+                        ImprimirTuplaItemValor(contaCorrente.CategoriaComMaiorGasto());
+                        PressioneParaContinuar();
+                        continue;
+                    case "4":
+                        ImprimirTuplaItemValor(contaCorrente.MesComMaiorGasto());
+                        PressioneParaContinuar();
+                        continue;
+                    case "5":
+                        ImprimirTotalDeGastos(contaCorrente.TotalDeGastos());
+                        PressioneParaContinuar();
+                        continue;
+                    case "6":
+                        ImprimirTotalDeRecebimentos(contaCorrente.TotalDeRecebimentos());
+                        PressioneParaContinuar();
+                        continue;
+                     case "7":
+                        ImprimirSaldo(contaCorrente.SaldoDeMovimentacoes());
+                        PressioneParaContinuar();
+                        continue;
+                    
+
+                }
+            }
 
         }
+
+
 
         private static List<Lancamento> RecuperarLancamentosApi()
         {
@@ -69,6 +109,60 @@ namespace contacorrente.app
 
             return lancamentos;
         }
+
+        private static void ImprimirMenuInicial()
+        {
+            Console.WriteLine("1 - exibir o log de movimentações de forma ordenada");
+            Console.WriteLine("2 - informar o total de gastos por categoria");
+            Console.WriteLine("3 - informar qual categoria cliente gastou mais");
+            Console.WriteLine("4 - informar qual foi o mês que cliente mais gastou");
+            Console.WriteLine("5 - quanto de dinheiro o cliente gastou");
+            Console.WriteLine("6 - quanto de dinheiro o cliente recebeu");
+            Console.WriteLine("7 - saldo total de movimentações do cliente");
+            Console.WriteLine("0 - Sair");
+        }
+
+        private static void ImprimirMovimentacaoOrdenada(List<Lancamento> lancamentos)
+        {
+            foreach (var lancamento in lancamentos)
+            {
+                Console.WriteLine($"{lancamento.Data:dd-MMM}\t{lancamento.Descricao.PadRight(28, ' ')}{lancamento.Valor:N2}\t\t\t{lancamento.Categoria}");
+            }
+
+        }
+
+        private static void PressioneParaContinuar()
+        {
+            Console.WriteLine("Pressione qualquer tecla para continuar");
+            Console.ReadLine();
+        }
+        private static void ImprimirTotalDeGastosPorCategoria(Tuple<string, decimal>[] gastosPorCategoria)
+        {
+            foreach (var item in gastosPorCategoria)
+            {
+                ImprimirTuplaItemValor(item);
+            }
+        }
+                private static void ImprimirSaldo(decimal valor)
+        {
+            Console.WriteLine($"Saldo movimentacoes: {valor:N2}");
+        }
+
+        private static void ImprimirTotalDeRecebimentos(decimal valor)
+        {
+            Console.WriteLine($"Total de Recebimentos: {valor:N2}");
+        }
+
+        private static void ImprimirTotalDeGastos(decimal valor)
+        {
+            Console.WriteLine($"Total de Gastos: {valor:N2}");
+        }
+
+        private static void ImprimirTuplaItemValor(Tuple<string, decimal> tupla)
+        {
+            Console.WriteLine($"{tupla.Item1.PadRight(20)}\t{tupla.Item2:N2}");
+        }
+
 
     }
 
